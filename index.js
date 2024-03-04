24d6555b2c662e105985fde02d52359e948c2768
azaiezsene@yahoo.fr <azaiezsene@yahoo.fr>
2024-03-04 18:25:16 +0100

    Added few comments in the image drawing section of charcater in index.js

--- index.js
+++ index.js
@@ -19,16 +19,16 @@ playerImage.src = "./images/characters/Premade_Character_48x48_01.png";
 console.log(image);
 //I wait for the image to load before drawing it to the canvas.
 image.onload = () => {
-  ctx.drawImage(image, -263, -800);
+  ctx.drawImage(image, -263, -800); //Starting position in the office map
   ctx.drawImage(
     playerImage,
-    (playerImage.width / 56) * 1,
-    0,
-    playerImage.width / 56,
-    playerImage.height / 20,
-    canvasElm.width / 2,
-    canvasElm.height / 2,
-    playerImage.width / 56,
-    playerImage.height / 20
+    (playerImage.width / 56) * 1, //Sprite x position (Virtual grid of the sprite sheet is  56 x 20)
+    0, // Sprite y position
+    playerImage.width / 56, // cropped size of character block
+    playerImage.height / 20, // cropped size of character block
+    canvasElm.width / 2, // x position on canvas
+    canvasElm.height / 2, // y position on canvas
+    playerImage.width / 56, // actual size of character block
+    playerImage.height / 20 // actual size of character block
   );
 };
