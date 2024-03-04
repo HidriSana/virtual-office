3d6a1d1c9a1a427fe9a556c2d0a3ee13fc9e605b
azaiezsene@yahoo.fr <azaiezsene@yahoo.fr>
2024-03-04 18:17:06 +0100

    Added character sprites grid + Placed player at front door, in front of the receptiondesk

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 The actual map was done with tiled and modern office tileset
+
+Player sprites grid is :
+
+Width : 56 tiles
+Height: 20 tiles
+
+X position is x _ playerimagewidth/56
+y position is y _ playerimage.height/20
--- index.js
+++ index.js
@@ -12,9 +12,23 @@ ctx.fillRect(0, 0, canvasElm.width, canvasElm.height);
 //Canvas does not support images from the local file system so we need to create an image object and set the source to the image file we want to use.
 const image = new Image();
 image.src = "./images/hidris-office-map-200-zoomed.png";
+//Create a new image object for the player
+const playerImage = new Image();
+playerImage.src = "./images/characters/Premade_Character_48x48_01.png";
 
 console.log(image);
 //I wait for the image to load before drawing it to the canvas.
 image.onload = () => {
-  ctx.drawImage(image, -300, -600);
+  ctx.drawImage(image, -263, -800);
+  ctx.drawImage(
+    playerImage,
+    (playerImage.width / 56) * 1,
+    0,
+    playerImage.width / 56,
+    playerImage.height / 20,
+    canvasElm.width / 2,
+    canvasElm.height / 2,
+    playerImage.width / 56,
+    playerImage.height / 20
+  );
 };
